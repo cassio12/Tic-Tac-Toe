80f238805273a70747deb7e22e73cfe308daf747
user <cassio12>
2021-04-15 22:05:17 -0300

    first commit

---
+++ src/components/square.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+function Square() {
+      return (
+        <button className="square">
+          {/* TODO */}
+        </button>
+      );
+    }
+
+export default Square;
